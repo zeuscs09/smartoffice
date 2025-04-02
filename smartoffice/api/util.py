88025529a189b3c816b335b8c9bafb79201ba993
tonjingjai <nattapong@jingjaisoft.com>
2025-04-02 18:02:47 +0700

    refactor: Enhance comment retrieval in API utility
    - Add ordering to the comment retrieval query to ensure comments are returned in descending order by creation date, improving the relevance of displayed comments.

--- smartoffice/api/util.py
+++ smartoffice/api/util.py
@@ -18,5 +18,6 @@ def get_comments(name,comment_type,reference_doctype):
         WHERE `reference_doctype` = %s 
         AND `reference_name` = %s 
         AND `comment_type` = %s
+        ORDER BY creation DESC
     """, (reference_doctype, name, comment_type) ,as_dict=True)
     return comments
